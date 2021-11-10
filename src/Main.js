31f02501c592c9c137cb764831172ce3451191e6
linkmiyu <dbwlsgus77@gmail.com>
2021-11-10 15:05:33 +0900

    master

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import './Main.css';
+import { HashRouter, Route } from 'react-router-dom'
+import About from './routes/About'
+import Home from './routes/Home'
+import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
+
+function Main(){
+   return(
+     <HashRouter>
+       <Navigation />
+       <Route path="/" exact={true} component={Home} />
+       <Route path='/about' component={About} />
+       <Route path="/movie-detail" component={Detail} />
+     </HashRouter>
+   )
+}
+export default Main;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+// import Movie from './Movie'
+// import Foo from './Foo'
+import App from './Main'
+
+ReactDOM.render(<App />, document.getElementById('root'))
